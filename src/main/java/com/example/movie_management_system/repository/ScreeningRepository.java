df0b5f81cc004a05bfc9ec2ca875c637d39e3b07
Zota Rares <rares.zota@stud.ubbcluj.ro>
2025-11-03 10:23:27 +0200

    another 2 Repositories

---
+++ src/main/java/com/example/movie_management_system/repository/ScreeningRepository.java
@@ -0,0 +1,9 @@
+package com.example.movie_management_system.repository;
+
+import com.example.movie_management_system.model.Screening;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScreeningRepository   extends JpaRepository<Screening, String> {
+}
