9607e1bab3cd0e3b40640c011925b5b613ad3e6d
Zota Rares <rares.zota@stud.ubbcluj.ro>
2025-11-03 10:29:04 +0200

    last 2 Repositories

---
+++ src/main/java/com/example/movie_management_system/repository/TheatreRepository.java
@@ -0,0 +1,8 @@
+package com.example.movie_management_system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TheatreRepository extends JpaRepository<TheatreRepository, String> {
+}
