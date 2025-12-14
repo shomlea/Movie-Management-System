0f380fd6b3df5227236545b5415b3400e9a7eb2c
shomlea <shomleatudor@gmail.com>
2025-12-14 19:21:40 +0200

    added and modified repository queries

--- src/main/java/com/example/movie_management_system/repository/TicketRepository.java
+++ src/main/java/com/example/movie_management_system/repository/TicketRepository.java
@@ -4,9 +4,11 @@ import com.example.movie_management_system.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket,Long> {
     Optional<Ticket> findBySeatIdAndScreeningId(Long seatId, Long screeningId);
+    List<Ticket> findByScreeningId(Long screeningId);
 }
